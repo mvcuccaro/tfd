23b77ce98f17dfb89d55e7ef1adee5ac79100257
mvcuccaro <mvcuccaro@users.noreply.github.com>
2018-03-02 18:03:57 -0500

    added js/jquery functions to populate presorted data - added column header click functionality to sort by column names

---
+++ public/js/people.js
@@ -0,0 +1,50 @@
+$(function(){
+	//do an initial sort at load
+	sortPeopleByColumn({
+		column: default_sort_column
+	});
+
+	$('.sortable_column').on('click', function(event){
+		sortPeopleByColumn({
+			column:event.currentTarget.id
+		});
+	});
+});
+
+/**
+ * Sort 
+ * @param  {[type]} arg_sort_info [description]
+ * @return {[type]}               [description]
+ */
+function sortPeopleByColumn(arg_sort_info){
+	var column = arg_sort_info.column
+	people.sort(function(a, b){
+		if( a[column] < b[column] ){
+			return -1;
+		}
+		if( a[column] > b[column] ){
+			return 1;
+		}
+		return 0;
+	});
+
+	//update the table with the sorted data
+	updatePeopleTable();
+	return true;
+}
+
+function updatePeopleTable(){
+	//remove current data from table:
+	$('#table_container').children(':not(#table_header)').remove();
+
+	//append people array to the data container
+	people.forEach(function(obj){
+		var row = '<div class="row">'
+			+ '<div class="col-sm-4">' + obj.first_name + '</div>'
+			+ '<div class="col-sm-4">' + obj.last_name + '</div>'
+			+ '<div class="col-sm-4">' + obj.favorite_color + '</div>';
+			+ '</div>';
+
+		$('#table_container').append(row);
+	});
+}
--- resources/views/people.blade.php
+++ resources/views/people.blade.php
@@ -3,8 +3,10 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/darkly/bootstrap.min.css">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="/js/people.js"></script>
 	<script type="text/javascript">
-		var people = JSON.parse('{!! $people !!}');
+		var people 				= JSON.parse('{!! $people !!}');
+		var default_sort_column = 'last_name';
 		console.log(people);
 	</script>
 	<title>tfd</title>
@@ -16,10 +18,12 @@
 				<div class="row">
 					<div class="col-sm-6 text-center"><h1>TFD Test People</h1></div>
 				</div>
-				<div class="row">
-					<div class="col-sm-3 bg-info">First Name</div>
-					<div class="col-sm-3 bg-info">Last Name</div>
-					<div class="col-sm-3 bg-info">Favorite color</div>
+				<div id="table_container">
+				<div class="row" id="table_header">
+					<div class="col-sm-4 bg-info sortable_column" id="first_name">First Name</div>
+					<div class="col-sm-4 bg-info sortable_column" id="last_name">Last Name</div>
+					<div class="col-sm-4 bg-info sortable_column" id="favorite_color">Favorite color</div>
+				</div>
 				</div>
 			</div>
 		</div>
