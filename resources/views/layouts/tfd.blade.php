7ba66a67c14f240c0afec57bcc59cdbed0c8208b
mvcuccaro <mvcuccaro@users.noreply.github.com>
2018-03-03 10:16:03 -0500

    added the master file since i didnt do that before the last commit ;]

---
+++ resources/views/layouts/tfd.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/darkly/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/css/people.css">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="/js/people.js"></script>
+	<script type="text/javascript">
+		var people 					= JSON.parse('{!! $people !!}');
+	</script>
+	<title>tfd</title>
+</head>
+<body class="bg-grey">
+	<div id="header">
+		<h1>Welcome to TFD</h1>
+	</div>
+	@yield('content')
+	<div id="#footer">
+		All pretend rights reserved
+	</div>
+</body>
+</html>
